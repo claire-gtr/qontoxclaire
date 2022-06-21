12b1adc70d1d52518e0cb6f4f98fcf45e697ea77
claire gautier <claire.gautier@edhec.com>
2022-06-21 11:26:17 +0200

    Polished tests

--- spec/routes/redirections_spec.rb
+++ spec/routes/redirections_spec.rb
@@ -1,7 +1,6 @@
 describe "Redirections", type: :request do
 
   context "redirects correctly" do
-
     it "redirects to the correct path with the correct status" do
       get "/de/company-creation"
       expect(response.status).to eq(301)
@@ -15,10 +14,9 @@ describe "Redirections", type: :request do
     end
 
     it "redirects to the correct path with the correct status and removes the trailing slash" do
-      get "/de/sme/firmenkonto-worauf-ist-zu-achten"
+      get "/de/sme/firmenkonto-worauf-ist-zu-achten/"
       expect(response.status).to eq(302)
       expect(response).to redirect_to("/de/open-an-account/firmenkonto")
     end
-    
   end
 end
